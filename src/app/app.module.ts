1106c422bcc4ed64fc798088388a2da388661fa8
shardul.sayankal <shardul.sayankal@coditas.com>
2022-06-15 17:21:33 +0530

    3/5 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteslistComponent } from './pages/noteslist/noteslist.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteCardComponent } from './note-card/note-card.component';
+import { NoteDetailsComponent } from './pages/note-details/note-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteslistComponent,
+    MainLayoutComponent,
+    NoteCardComponent,
+    NoteDetailsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/noteslist/noteslist.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Note } from 'src/app/shared/note.model';
+import { NotesService } from 'src/app/shared/notes.service';
+
+@Component({
+  selector: 'app-noteslist',
+  templateUrl: './noteslist.component.html',
+  styleUrls: ['./noteslist.component.scss'],
+})
+export class NoteslistComponent implements OnInit {
+  notes: Note[] = new Array<Note>();
+
+  constructor(private noteService: NotesService) {}
+
+  ngOnInit(): void {
+    this.notes = this.noteService.getAll();
+  }
+
+  deleteNote(id: number) {
+    this.noteService.delete(id);
+  }
+}
